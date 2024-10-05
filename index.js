2a8a866078fde5905d3258a79bfe3002efc2e6ba
+ <kibetg984@gmail.com>
2024-10-05 09:02:57 +0300

    Added the title cased dunction to work on the strings

--- index.js
+++ index.js
@@ -12,5 +12,17 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    // Split the tutorial title into words
+    let words = tutorial.split(' ');
+
+    // Capitalize the first letter of each word while preserving existing capitalization
+    let capitalizedWords = words.map(word => {
+      // If the word is fully uppercase (like "OO", "API"), leave it unchanged except for first letter
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+
+    // Join the words back into a string
+    return capitalizedWords.join(' ');
+  });
+};
